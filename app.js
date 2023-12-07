bbd7146e004e2ff00f7e918676bf05d83938a1b7
Jaramillo-Alejo <danijaramillo26@yahoo.com>
2023-12-07 15:07:49 -0600

    adding changes with API.

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+document.addEventListener("click", function (event) {
+  // Checking if the button was clicked
+  if (!event.target.matches("#button")) return;
+
+  fetch("https://official-joke-api.appspot.com/random_joke")
+    .then((response) => response.json())
+    .then((data) => renderJoke(data));
+});
+
+function renderJoke(data) {
+  const setup = document.getElementById("setup");
+  const punchline = document.getElementById("punchline");
+  setup.innerHTML = data.setup;
+  punchline.innerHTML = data.punchline;
+}
+
+// https://www.shecodes.io/athena/10692-what-is-an-api-key-and-how-to-use-it-in-javascript ;using an API key
